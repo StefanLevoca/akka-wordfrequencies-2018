7832e3e72444f3a57c866256fe4e7391b4f42bb3
Robert Novotny <novotnr0@gmail.com>
2019-01-15 10:13:10 +0100

    Aggregate all frequencies that arrive from sentence counting actors

--- src/main/java/com/github/novotnyr/akka/MasterActor.java
+++ src/main/java/com/github/novotnyr/akka/MasterActor.java
@@ -7,6 +7,7 @@ import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.routing.RoundRobinPool;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MasterActor extends AbstractActor {
@@ -15,11 +16,17 @@ public class MasterActor extends AbstractActor {
 	private ActorRef sentenceCounter = getContext().actorOf(SentenceCountActor.props()
 			.withRouter(new RoundRobinPool(3)));
 
+	private Map<String, Integer> allFrequencies = new HashMap<>();
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
 				.match(String.class, sentence -> sentenceCounter.tell(sentence, getSelf()))
-				.match(Map.class, frequencies -> logger.info(frequencies.toString()))
+				.match(Map.class, frequencies -> {
+					allFrequencies = MapUtils.aggregate(frequencies, allFrequencies);
+					logger.info(allFrequencies.toString());
+				})
 				.build();
 	}
 
